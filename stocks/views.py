c0c21e3b6a144676ccdc2f42bbaa6eb5a3ccbb93
IdioticMarlin22 <jcrantfo@comcast.net>
2023-07-18 19:56:02 -0400

    Created /utils in stocks app. Moved in apiconnection.py and tradingAlgorithm.py

--- stocks/views.py
+++ stocks/views.py
@@ -3,7 +3,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .apiconnection import getStockData
+from .utils.apiconnection import getStockData
 
 
 @api_view(['POST'])
